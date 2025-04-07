bf500e9157750fce6dc36e21af7468156fd0e54d
zetachua <zetachua246@gmail.com>
2025-04-07 17:55:19 +0800

    Update InteractiveInvestors.tsx

--- components/InteractiveInvestors.tsx
+++ components/InteractiveInvestors.tsx
@@ -622,7 +622,7 @@ async function endSession() {
             <div className="h-full justify-center items-center flex flex-col gap-8 w-[500px] self-center"style={{backgroundColor:'rgba(255,255,255,0.2)',borderRadius:'50px',padding:'2rem',maxHeight:'30%'}}>
               <div className="flex flex-col gap-2 w-full" style={{position:'relative'}} >
                  <Select
-                    placeholder="Select AI Modelsss"
+                    placeholder="Select AI Models Okay"
                     size="md"
                     value={selectedModel}
                     onChange={(e) => {
